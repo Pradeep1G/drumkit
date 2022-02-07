c092f5e4c21ecae979a8071543d23600ca614055
Pradeep1G <98323512+Pradeep1G@users.noreply.github.com>
2022-02-07 12:51:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,37 +11,37 @@ function funswitch(ke){
 
   switch (ke) {
     case "w":
-      var sund = new Audio("Audio/tom-1.mp3");
+      var sund = new Audio("tom-1.mp3");
       sund.play();
       break;
 
     case "a":
-      var sund = new Audio("Audio/tom-2.mp3");
+      var sund = new Audio("tom-2.mp3");
       sund.play();
       break;
 
     case "s":
-      var tom3 = new Audio("Audio/tom-3.mp3");
+      var tom3 = new Audio("tom-3.mp3");
       tom3.play();
       break;
 
     case "d":
-      var sund = new Audio("Audio/tom-4.mp3");
+      var sund = new Audio("tom-4.mp3");
       sund.play();
       break;
 
     case "j":
-      var sund = new Audio("Audio/snare.mp3");
+      var sund = new Audio("snare.mp3");
       sund.play();
       break;
 
     case "k":
-      var sund = new Audio("Audio/crash.mp3");
+      var sund = new Audio("crash.mp3");
       sund.play();
       break;
 
     case "l":
-      var sund = new Audio("Audio/kick-bass.mp3");
+      var sund = new Audio("kick-bass.mp3");
       sund.play();
       break;
 
